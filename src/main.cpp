4589c827bed7b7f2b1d7e4dcb845914c4389ef38
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2012-07-07 18:05:56 +0300

    added more pseudo test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,27 @@
 #include "includes/simplenote.hpp"
+#include "includes/note.hpp"
+
+#include <iostream>
 
 using namespace std;
 
 int main(){
     Simplenote a("", "");
+
+    set<string> tags = {"abd", "foo"};
+    Note foo, x;
+    foo.content = "notecontent";
+    foo.tags = tags;
+
+    x = a.create_note(foo);
+
+    cout<<"\nDeleted: "<<x.deleted;
+    set<string>::iterator i;
+
+    for(i=x.tags.begin(); i!=x.tags.end(); i++){
+        cout<<*i;
+    }
+    for(i=x.systemtags.begin(); i!=x.systemtags.end(); i++){
+        cout<<*i;
+    }
 }
